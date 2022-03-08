06658bf629a7c47c2bebaaf0c90bc2c2e82cb33e
Admin <Admin@DESKTOP-9833F0D>
2022-03-07 21:08:45 -0800

    1st commit

---
+++ area/src/area/Rectangle.java
@@ -0,0 +1,13 @@
+package area;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double length=4.5;
+double width=8.0;
+double area=length*width;
+System.out.println("Area of rectangle is:"+area);
+	}
+
+}
---
+++ area/src/area/Triangle.java
@@ -0,0 +1,12 @@
+package area;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float b=4,h=13,area;
+area=(b*h)/2;
+System.out.println("Area of triangle is:"+area);
+	}
+
+}
